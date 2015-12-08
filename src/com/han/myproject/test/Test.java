17e1443dd440af0c750235bb740a296b6e5d624d
han.zhang <cultivator1026@126.com>
2015-12-08 17:13:17 +0800

    151208 没什么更新

--- src/com/han/myproject/test/Test.java
+++ src/com/han/myproject/test/Test.java
@@ -1,22 +1,19 @@
 package com.han.myproject.test;
 
-import android.util.Log;
-
-
 
 public class Test {
 	public static void main(String[] args) {
-		String userName = "13873103362";
-		String sKey = userName + userName.substring(userName.length()-5);
-		String encLoginPwd = "";
-		try {
-			encLoginPwd = EncryptUtil.encrypt("123456", sKey);
-			Log.e("password", encLoginPwd);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-
+			System.out.println(find(1232421412));
 	}
+	
+	
+	
+	public static <T> T find(T t){
+		System.out.println(t.getClass());
+		System.out.println(t.hashCode());
+		return t;
+	}
+	
+	
 }
+
